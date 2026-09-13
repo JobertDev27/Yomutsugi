b914c16335640bc22760f59d2cabb10e3beadfa2
jobertdev27 <jobertdev27@gmail.com>
2026-09-13 17:08:39 +0800

    feat: display user shows from supabase

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,20 +4,40 @@ import type { Show } from "../types/type.ts"
 import Header from "../components/Header";
 import { useAuth } from "../hooks/useAuth";
 import { useEffect, useState } from "react";
+import {supabase} from "../utils/supabase.ts"
+import ShowCard from "../components/ShowCard.tsx";
 
 export const Route = createFileRoute('/')({
     component: Index,
 })
 
+type UserShow = {
+    id: number;
+    user_id: string;
+    mal_id: number;
+    category: number | null;
+    shows: Show;
+};
+
 function Index():React.ReactNode {
     const claims : JwtPayload | null = useAuth()
-    
-    const [userLib, setUserLib] = useState<Show>()
+
+    const [userLib, setUserLib] = useState<UserShow[]>([])
 
     useEffect(() => {
 	if (!claims) return;
 
-	// TODO fetch from database using join to merge cache_shows and user_shows to lessen rate limit 
+	const getShows = async () => {
+	    const { data, error } = await supabase
+	    .from("user_shows")
+	    .select(`*, shows (title, cover)`)
+	    .eq("user_id", claims.sub);
+
+	    console.log(data)
+	    if (data) setUserLib(data);
+	    if (error) console.error("userLib:", error);
+	}
+	getShows()
     },[])
 
     // If user is logged in, show welcome screen
@@ -28,6 +48,9 @@ function Index():React.ReactNode {
 	    <div>
 	    <h1>Welcome!</h1>
 	    <p>You are logged in as: {claims.email}</p>
+	    <main className="grid lg:grid-cols-8 md:grid-cols-4 grid-cols-2 gap-3">
+	    {userLib.map((s, key) => <ShowCard key={key} show={s.shows} />)}
+	    </main>
 	    </div>
 	    </>
 	)
@@ -36,7 +59,7 @@ function Index():React.ReactNode {
 	<>
 	<Header />
 	<div>
-	<p>Hello World</p>
+	<p>You are not logged in</p>
 	</div>
 	</>
     )
